0b73e74d023b49cb05dcdafbe2abb6dea36a9893
Tobias Letschka <Tobias_Letschka@web.de>
2016-06-12 20:32:14 +0200

    small changes

--- src/main/java/de/tuxftp/application/ApplicationFtpClient.java
+++ src/main/java/de/tuxftp/application/ApplicationFtpClient.java
@@ -1,11 +1,13 @@
 package de.tuxftp.application;
 
+import static java.lang.System.in;
+
 import java.util.Scanner;
+
+import org.apache.commons.lang.StringUtils;
 
 import de.tuxftp.application.ftpClients.AnonymousSession;
 import de.tuxftp.application.ftpClients.UserPasswordSession;
-
-
 
 public class ApplicationFtpClient {
 
@@ -13,28 +15,34 @@ public class ApplicationFtpClient {
 		System.out.println("Which kind of connection should be established?");
 		System.out.println("typ 1 for a anonymous session");
 		System.out.println("typ 2 for a user+password session");
-		
-		Scanner scanner = new Scanner(System.in);
-		int input;
-		
-		input = scanner.nextInt();
-		
-		switch(input) {
-		case(1): ApplicationFtpClient.testForServerWithAnonyoumsAccount();
+
+		Scanner scanner = new Scanner(in);
+		int input = 0;
+		try {
+			input = scanner.nextInt();
+
+			switch (input) {
+			case (1):
+				ApplicationFtpClient.testForServerWithAnonyoumsAccount();
 				break;
-		case(2): ApplicationFtpClient.testforServerWithUserPasswortAccount();
+			case (2):
+				ApplicationFtpClient.testforServerWithUserPasswortAccount();
 				break;
+			}
+		} finally {
+			scanner.close();
 		}
-		
-	
-	
+
 	}
+
 	private static void testforServerWithUserPasswortAccount() {
-		new UserPasswordSession("pr0-talk.de",21,"web16","Password");
+		new UserPasswordSession("pr0-talk.de", 21, "web16", "Password");
 		
+
 	}
+
 	static void testForServerWithAnonyoumsAccount() {
-		new AnonymousSession("ftp2.de.debian.org",21);  						
+		new AnonymousSession("ftp2.de.debian.org", 21);
 	}
 
 }
